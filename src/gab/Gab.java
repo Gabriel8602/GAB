a95895663dc56a1e07ae38d515aaa107ad4c5d5b
Gabriel8602 <gabrielcostanos12@gmail.com>
2025-07-29 13:40:05 +0800

    push me

---
+++ src/gab/Gab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gab;
+
+/**
+ *
+ * @author PC7
+ */
+public class Gab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
